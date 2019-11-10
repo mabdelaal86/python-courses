27c19b0dc43aad2098310b195e561faaee3e4ecb
mohammed abdelaal <mabdelaal@ictcube.com>
2019-11-10 18:41:46 +0200

    add example 3

---
+++ Flask/1/example_3/example_3.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask('example_3')
+
+
+@app.route('/')
+@app.route("/index")
+def index():
+    return render_template('index.html')
+
+
+@app.route("/welcome")
+def welcome():
+    user = "Hamdy"
+    return render_template('welcome.html', user_name=user)
+
+
+@app.route("/welcome-dict")
+def welcome_dict():
+    user = {"name": "Fatma", "age": 23, "gender": "Female"}
+    return render_template('welcome-dict.html', user=user)
+
+
+@app.route("/welcome-list")
+def welcome_list():
+    students = ["Ahmad", "Samy", "Yasser", "Hoda"]
+    return render_template('welcome-list.html', names=students)
+
+
+app.run(debug=True)
